98e81e08af8b41969e216afb033f244e34f704ad
Faty <fatoudialloo95@gmail.com>
2024-04-17 11:07:35 +0200

    modifs

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-document.querySelector('#btn').addEventListener('click', function(){
-    const user = {
-        departure: document.querySelector('#search-departure').value,
-        arrival: document.querySelector('#search-arrival').value,
-        date: document.querySelector('#search-date').value,
-       };
-
- fetch('', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({user}),
- }).then(response => response.json())
-    .then(data => {
-        if (data.result) {
-            window.location.assign('.html');
-          }
-    })
+let depart = document.querySelector('#search-departure').value;
+let arrivee = document.querySelector('#search-arrival').value;
+let date = document.querySelector('#search-date').value
+document.querySelector('#search').addEventListener('click', function(){
+    const Trip = [
+        {
+        departure: depart,
+        arrival: arrivee,
+        date: date
+    }
+    ];
+  fetch('http://localhost:3000/trips')
+ .then(response => response.json())
+ .then(data => {
+    for(let i = 0; i < data.length; i++) {
+    console.log(data.departure);
+   document.querySelector('#content-right').innerHTML += 
+   <div class="cityContainer">
+      <p class="name">${data.departure}</p>
+    </div> 
+    }
+ });
+// console.log(depart)
+//  fetch('http://localhost300/trips', {
+//     method: 'GET',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({trip}),
+//  }).then(response => response.json())
+// fetch('http://localhost:3000/trips')
+//  .then(response => response.json())
+//     .then(data => {
+//         for(let i = 0; i < data.length; i++) {
+//         if (data.departure) {
+//            console.log(data.departure);
+//           }
+//         }
+//     })
 })
