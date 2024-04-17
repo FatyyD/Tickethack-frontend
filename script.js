69834dfe5da8d7a578674175523425c42c1a33e4
amine_benmbark@hotmail.com <dortmund91000@hotmail.com>
2024-04-17 14:47:41 +0200

    pp

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-// let depart = document.querySelector('#search-departure');
-// let arrivee = document.querySelector('#search-arrival');
-// let date = document.querySelector('#search-date');
-
 document.querySelector('#search').addEventListener('click', function(){
     // console.log(depart.value);
     // console.log(arrivee.value);
     // console.log(date.value);
-    //     const Trip = [
-    //         {
-    //         departure: depart,
-    //         arrival: arrivee,
-    //         date: date
-    //     }
-    //     ];
-    let depart = document.querySelector('#search-departure').value;
-    let arrivee = document.querySelector('#search-arrival');
-    let date = document.querySelector('#search-date');
+let depart = document.querySelector('#search-departure').value
+let arrivee = document.querySelector('#search-arrival').value
+let date = document.querySelector('#search-date');
+        const Trip = [
+            {
+            departure: depart,
+            arrival: arrivee,
+            date: date
+        }
+        ];
+        
     
-    fetch('http://localhost:3000/trips')
+    
+    fetch(`http://localhost:3000/trips/${depart}/${arrivee}`)
      .then(response => response.json())
      .then( data => {
-    //  for(let i = 0; i < data.length; i++) {
-        if(depart === data.departure)
-            console.log(data);
-        else
+        // console.log(data);
+     for(let i = 0; i < data.trips.length; i++) {
+        // console.log("dd");
+        // console.log(data.trips[i].departure);
+        if(depart === data.trips[i].departure){
+           document.querySelector('#content-right').innerHTML += 
+        ` <div class="cityContainer">
+          <p class="name">${data.trips[i].departure}</p>
+          <p class="name">${data.trips[i].arrival}</p>
+
+        </div> ` 
+     }
+            // console.log(data.trips[i].departure);
+        else{
             console.log("plp");
-     
-    //    document.querySelector('#content-right').innerHTML += 
-    //    ` <div class="cityContainer">
-    //       <p class="name">${data.departure}</p>
-    //     </div> ` 
-    //  }
+      }
+    }
      });
     
     // console.log(depart)
